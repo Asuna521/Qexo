283a6ef0b5054121bf7a8405b5b61d4701e974b3
Asuna521 <57357354+Asuna521@users.noreply.github.com>
2026-07-25 02:57:53 +0800

    恢复 fork 的 vercel.sh 原版

--- vercel.sh
+++ vercel.sh
@@ -3,6 +3,6 @@
 # Install dependencies
 python3 -m pip install --break-system-packages -r requirements.txt
 
-# Migrate database (use --fake to skip actual schema changes on existing MongoDB data)
+# Migrate database
 python3 manage.py makemigrations
-python3 manage.py migrate --fake --noinput
+python3 manage.py migrate --noinput
