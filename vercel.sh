18b07080c10370f1c300c4e206cb59ac33ea5794
Asuna521 <57357354+Asuna521@users.noreply.github.com>
2026-07-25 01:51:34 +0800

    fix: use --fake migrate for existing MongoDB data

--- vercel.sh
+++ vercel.sh
@@ -3,6 +3,6 @@
 # Install dependencies
 python3 -m pip install --break-system-packages -r requirements.txt
 
-# Migrate database
+# Migrate database (use --fake to skip actual schema changes on existing MongoDB data)
 python3 manage.py makemigrations
-python3 manage.py migrate --noinput
+python3 manage.py migrate --fake --noinput
